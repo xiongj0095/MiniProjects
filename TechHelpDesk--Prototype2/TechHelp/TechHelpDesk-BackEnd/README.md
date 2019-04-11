0ab8582f533735432b21d1215d1523dcf449e175
Joey <joey-xiong@hotmail.com>
2019-04-11 13:14:54 -0500

    updated techhelp

---
+++ TechHelpDesk--Prototype2/TechHelp/TechHelpDesk-BackEnd/README.md
@@ -0,0 +1,1 @@
+This is the repository for the backend code for the helpdesk project.
---
+++ TechHelpDesk--Prototype2/TechHelp/TechHelpDesk-BackEnd/php/load_ticket.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+include ('includes/dbconnect.php');
+global $db;
+$userid = $_SESSION["userid"];
+//echo "User id is " . $userid;
+// select title, date, custnotes, technotes from tickets join ticket_assignments on ticket_assignments.ticketid = tickets.id where ticket_assignments.userid = 5;
+$result = mysqli_query($db, "SELECT tickets.id, title, date, custnotes, technotes 
+                            FROM `techhelpdb`.`tickets` JOIN `techhelpdb`.`ticket_assignments` on ticket_assignments.ticketid = tickets.id 
+                            WHERE ticket_assignments.userid = '$userid'");
+if (mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+       echo $row["custnotes"] . "\n";
+    }
+} else {
+    echo "no entries!\n";
+}
+
+?>
